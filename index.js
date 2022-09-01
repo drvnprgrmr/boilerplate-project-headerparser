bbf8574941dcc1a63b9d0bba9a5dd2bf6602b043
Ibukun Ekunwe <67306305+drvnprgrmr@users.noreply.github.com>
2022-09-01 11:48:48 +0000

    Done with project

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+    const {
+        "x-forwarded-for": ipaddress,
+        "accept-language": language,
+        "user-agent": software
+    } = req.headers
+    res.json({
+        ipaddress: ipaddress,
+        language: language,
+        software: software
+    })
+    
+})
+    
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
